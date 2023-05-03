1e6dd7df48a44e15c2877dda9bc5feae90898b1a
James <jkeezer@pm.me>
2023-05-02 23:04:59 -0400

    Complete topicsSlice, addTopic, exports

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,10 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"
-
-export const addTopic = () => {
-
-}
+import { createSlice } from "@reduxjs/toolkit";
 
 export const topicsSlice = createSlice({
-    name: 'topicsSlice',
-    topics: {},
-})
+  name: "topicsSlice",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      return {
+        ...state,
+        [action.payload.id]: {
+          id: action.payload.id,
+          name: action.payload.name,
+          icon: action.payload.icon,
+          quizIds: action.payload.quizIds
+        }
+      };
+    }
+  }
+});
+
+export const selectTopics = state => state.topics;
+
+export const { addTopic } = topicsSlice.actions;
+
+export default topicsSlice.reducer;
