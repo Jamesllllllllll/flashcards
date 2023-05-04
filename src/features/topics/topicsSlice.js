3ac5246caa12f9f537da40125f305aea7b214b30
James <jkeezer@pm.me>
2023-05-04 00:01:43 -0400

    Show quizzes/topic. selector state.topics.topics??

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,29 +1,16 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
-export const addToQuizIds = createAsyncThunk(
-  'topics/addToQuizIds',
-  async (addQuiz, { dispatch }) => {
-    const response = await dispatch(addQuiz);
-    return response;
-  }
-  //...state,
-  //[action.payload.id]: {
-  //  quizIds: state.topics.quizIds.push(action.payload.quizIds)
-  // Step 11: Something that adds a quiz's id to the quizIds  
-  // array of the topic the quiz is associated with.
-  // This action will receive a payload of the form
-  // {quizId: '123', topicId: '456'}.
-);
+import { createSlice } from "@reduxjs/toolkit";
 
 export const topicsSlice = createSlice({
   name: "topicsSlice",
   initialState: {
-    topics: {
-      // Placeholder must be here to not throw error. Issue with quizIds not defined
-      id: 1,
-      name: "Placeholder Topic",
-      icon: "default",
-      quizIds: []
+    topics: { // Placeholder must be here to not throw error. Issue with quizIds not defined
+      '1': {
+        id: '1',
+        name: "Placeholder Topic",
+        icon:
+          "https://static-assets.codecademy.com/skillpaths/react-redux/redux-quiz-app/shuttlecock.svg",
+        quizIds: ['456','789']
+      }
     }
   },
   reducers: {
@@ -34,15 +21,27 @@ export const topicsSlice = createSlice({
           id: action.payload.id,
           name: action.payload.name,
           icon: action.payload.icon,
-          quizIds: [],
+          quizIds: []
         }
       };
     },
+    addToQuizIds: (state, action) => {
+      return {
+        ...state,
+        [action.payload.quizId]: {
+          quizIds: state.topics.quizIds.push(action.payload.quizIds)
+        }
+        // Step 11: Something that adds a quiz's id to the quizIds
+        // array of the topic the quiz is associated with.
+        // This action will receive a payload of the form
+        // {quizId: '123', topicId: '456'}.
+      };
+    }
   }
 });
 
-export const selectTopics = (state) => state.topics;
+export const selectTopics = (state) => state.topics.topics;
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addToQuizIds } = topicsSlice.actions;
 
 export default topicsSlice.reducer;
