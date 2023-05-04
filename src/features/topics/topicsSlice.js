e8c184af0514a25f4b1f3b7ca1dc91d7b8029e1c
James <jkeezer@pm.me>
2023-05-04 00:35:08 -0400

    Adding topics works. Perhaps ineligantly.

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -3,25 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 export const topicsSlice = createSlice({
   name: "topicsSlice",
   initialState: {
-    topics: { // Placeholder must be here to not throw error. Issue with quizIds not defined
-      '1': {
-        id: '1',
+    topics: {
+      // Placeholder must be here to not throw error. Issue with quizIds not defined
+      1: {
+        id: "1",
         name: "Placeholder Topic",
         icon:
           "https://static-assets.codecademy.com/skillpaths/react-redux/redux-quiz-app/shuttlecock.svg",
-        quizIds: ['456','789']
+        quizIds: ["456", "789"]
       }
     }
   },
   reducers: {
     addTopic: (state, action) => {
       return {
-        ...state.topics,
-        [action.payload.id]: {
-          id: action.payload.id,
-          name: action.payload.name,
-          icon: action.payload.icon,
-          quizIds: []
+        ...state,
+        topics: {
+          ...state.topics,
+          [action.payload.id]: {
+            id: action.payload.id,
+            name: action.payload.name,
+            icon: action.payload.icon,
+            quizIds: []
+          }
         }
       };
     },
