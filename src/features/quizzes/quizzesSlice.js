363363831910ace60b5b8f0dcceedf0608302733
James <jkeezer@pm.me>
2023-05-03 22:06:09 -0400

    Add quizzes to store and addToQuizIds action

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,18 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { addToQuizIds } from "../topics/topicsSlice";
+
+// Fix function createQuiz below
+export const createQuiz = createAsyncThunk(
+  'quizzes/createQuiz',
+  async (quiz, thunkApi) => {
+    const response = await thunkApi.dispatch(addQuiz);
+    // await thunkApi.dispatch(addToQuizIds); 
+  }
+);
 
 export const quizzesSlice = createSlice({
   name: "quizzesSlice",
   initialState: {
     quizzes: {
-
+      456: {
+        id: "456",
+        topicId: "227fad11-9788-4825-a255-3a0f2de22c61",
+        name: "quiz for example topic",
+        cardIds: ["789", "101", "102"]
+      }
     }
   },
   reducers: {
     addQuiz: (state, action) => {
-        return {
-
+      return {
+        ...state,
+        [action.payload.id]: {
+          id: action.payload.id,
+          name: action.payload.name,
+          topicId: action.payload.topicId,
+          cardIds: action.payload.cardIds
         }
-    }
+      };
+    },
+  },
+  failedToSaveQuiz: false,
+  isSavingQuiz: false,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createQuiz.pending, (state) => {
+        state.isSavingQuiz = true;
+        state.failedToSaveQuiz = false;
+      })
+      .addCase(createQuiz.fulfilled, (state, action) => {
+        state.isSavingQuiz = false;
+        state.failedToSaveQuiz = false;
+        state.quizzes = action.payload; // This is probably wrong
+      })
+      .addCase(createQuiz.rejected, (state) => {
+        state.isSavingQuiz = false;
+        state.failedToSaveQuiz = true;
+      })
   }
 });
 
--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -3,11 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 export const topicsSlice = createSlice({
   name: "topicsSlice",
   initialState: {
-    topics: { // Placeholder must be here to not throw error. Issue with quizIds not defined
-        id: 1,
-        name: 'Placeholder Topic',
-        icon: 'default',
-        quizIds: [],
+    topics: {
+      // Placeholder must be here to not throw error. Issue with quizIds not defined
+      id: 1,
+      name: "Placeholder Topic",
+      icon: "default",
+      quizIds: []
     }
   },
   reducers: {
@@ -18,8 +19,20 @@ export const topicsSlice = createSlice({
           id: action.payload.id,
           name: action.payload.name,
           icon: action.payload.icon,
-          quizIds: [],
+          quizIds: []
         }
+      };
+    },
+    addToQuizIds: (state, action) => {
+      return {
+        ...state,
+        [action.payload.id]: {
+          quizIds: state.topics.quizIds.push(action.payload.quizIds)
+        }
+       // Step 11: Something that adds a quiz's id to the quizIds  
+       // array of the topic the quiz is associated with.
+       // This action will receive a payload of the form
+       // {quizId: '123', topicId: '456'}.
       };
     }
   }
@@ -27,6 +40,6 @@ export const topicsSlice = createSlice({
 
 export const selectTopics = (state) => state.topics;
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addToQuizIds } = topicsSlice.actions;
 
 export default topicsSlice.reducer;
