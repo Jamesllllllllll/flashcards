cbaeb021cad27a3ac871ea5aa1f8887c289056cf
James <jkeezer@pm.me>
2023-05-05 01:03:05 -0400

    Tweak styling

--- src/app/App.js
+++ src/app/App.js
@@ -16,6 +16,7 @@ import ROUTES from "./routes";
 
 export default function App() {
   return (
+    <>
     <Router>
       <nav>
         <ul>
@@ -46,6 +47,14 @@ export default function App() {
         </Route>
       </Switch>
     </Router>
+    <footer>
+    <div id="codecademy">
+        <p>This is a <a href="https://codecademy.com" target="_blank">Codecademy</a> project created by <a href="https://github.com/jamesllllllllll" target="_blank">James</a> - April 2023</p>
+        <p><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" stylename="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a></p>
+        <p>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>
+      </div>
+    </footer>
+    </>
   );
 }
 
